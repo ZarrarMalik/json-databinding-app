212366208105a026ba74583d016ed3ddc87b9f86
zarrar <zarrarmalik81@gmail.com>
2020-05-19 16:16:32 +0100

    Refactor main class

---
+++ json-databinding-app/src/main/java/com/zarrar/malik/json_databinding_app/DataBinding.java
@@ -0,0 +1,13 @@
+package com.zarrar.malik.json_databinding_app;
+
+/**
+ * Hello world!
+ *
+ */
+public class DataBinding 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
